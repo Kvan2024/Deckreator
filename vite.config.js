c2a33ff80ecd7a2dacca897148f25a0dde8952b0
Kevin Van Ausdall <kvanausdall@comcast.net>
2025-06-18 09:44:41 -0700

    Updated routing config

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,12 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
-      '/api': 'http://deckreator.netlify.app',
-      '/auth': 'http://deckreator.netlify.app',
-      '/auth/login': 'http://deckreator.netlify.app',
-      '/register': 'http://deckreator.netlify.app',
-      '/cards': 'http://deckreator.netlify.app',
-      '/cards/search': 'http://deckreator.netlify.app',
+      '/api': 'https://deckreator.onrender.com',
+      '/auth': 'https://deckreator.onrender.com',
+      '/auth/login': 'https://deckreator.onrender.com',
+      '/register': 'https://deckreator.onrender.com',
+      '/cards': 'https://deckreator.onrender.com',
+      '/cards/search': 'https://deckreator.onrender.com',
     }
   }
 })
